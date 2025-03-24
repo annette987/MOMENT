01a9ec6b569d63872f83bfb9e77f6f5887dde152
Annette Spooner <a.spooner@unsw.edu.au>
2025-03-24 14:01:11 +1100

    Adding multilabel classification

--- R/perf_measures.R
+++ R/perf_measures.R
@@ -54,7 +54,7 @@ PerformanceMeasures = R6::R6Class("PerformanceMeasures",
 					sd(BBmisc::vnapply(split(perf.test, group), mean), na.rm = TRUE)
 				})
 				
-			if (task_type == TASK_SURV) {
+			if (task_type == "surv") {
 				cindex.na = mlr::setAggregation(cindex, test.mean_narm)
 				cindex.sdna = mlr::setAggregation(cindex, test.sd_narm)
 				cindex_grp.na = mlr::setAggregation(cindex, testgroup.mean_narm)
@@ -63,7 +63,7 @@ PerformanceMeasures = R6::R6Class("PerformanceMeasures",
 				cindex.uno.sdna = mlr::setAggregation(cindex.uno, test.sd_narm)
 				self$measures = list(cindex.na, cindex.sdna, cindex_grp.na, cindex_grp.sdna, cindex.uno.na, cindex.uno.sdna)
 #				self$measures = list(cindex.na, cindex.sdna, cindex.uno.na, cindex.uno.sdna)
-			} else {
+			} else if (task_type == "classif" ){
 				acc.na = mlr::setAggregation(mlr::acc, test.mean_narm)
 				acc.sdna = mlr::setAggregation(mlr::acc, test.sd_narm)
 				multiclass.aunu.na = mlr::setAggregation(mlr::multiclass.aunu, test.mean_narm)
@@ -74,6 +74,8 @@ PerformanceMeasures = R6::R6Class("PerformanceMeasures",
 				} else {
 					self$measures = list(acc.na)
 				}
+			} else if (task_type == "multilabel" ){
+				self$measures = list(multilabel.ppv, multilabel.tpr, multilabel.f1, multilabel.acc, multilabel.subset01, multilabel.hamloss)
 			}
 		}
 ))		
