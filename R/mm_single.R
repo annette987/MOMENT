49336b865a0e23a807487910e99b9a99479a921a
Annette Spooner <a.spooner@unsw.edu.au>
2025-03-28 13:03:44 +1100

    Adding multilabel classification

--- R/mm_single.R
+++ R/mm_single.R
@@ -16,7 +16,6 @@ MM_Single = R6::R6Class("MM_Single",
 	inherit = MM_Model,
 	public = list(
 		single_results = list(),
-		model_type = "classif",
 		
     #' @description 
 		#' Create a new MM_Single object.
@@ -46,7 +45,6 @@ MM_Single = R6::R6Class("MM_Single",
 		#' @export
 		initialize = function(config, task_type = "classif", predict_type = "response", concat = FALSE, balance = FALSE, validate = FALSE, filter_zeroes = 90.0, filter_missings = 50.0, filter_corr = FALSE, filter_var = FALSE) {
 			super$initialize(config, task_type, predict_type, "hard", NULL, concat, balance, validate, filter_zeroes, filter_missings, filter_corr, filter_var)
-			self$model_type = model_type
 		},		
 
 						
@@ -73,12 +71,12 @@ MM_Single = R6::R6Class("MM_Single",
 		
 		get_model_results = function(res, task)
 		{
-			results = MM_Results$new(self$classes, self$tasks, self$measures, self$model_type, self$decision)
+			results = MM_Results$new(self$classes, self$tasks, self$measures, self$task_type, self$decision)
 			
 			for (i in 1:length(res$models)) 
 			{
 				results$save_predictions(res$pred, task, res$models[[i]])
-				results$save_features(res$models[[i]], task, self$model_type, i)
+				results$save_features(res$models[[i]], task, self$task_type, i)
 			}
 			results$complete()
 			return(results)
@@ -94,7 +92,7 @@ MM_Single = R6::R6Class("MM_Single",
 		#' @export
 		learn = function(active_learners) 
 		{
-			learners = Learners$new(self$model_type)
+			learners = Learners$new(self$task_type)
 			base_filters = learners$base_filters
 			base_learners = learners$base_learners
 
@@ -105,7 +103,7 @@ MM_Single = R6::R6Class("MM_Single",
 					for (baselrn in base_learners) {
 						if (bitwAnd(active_learners, baselrn$code) || bitwAnd(active_learners, LRN_ALL_MODELS)) {
 							targets = getTaskTargetNames(self$tasks[[i]])
-							lrn = learners$create_learner(targets, baselrn, NULL, self$model_type, self$decision, TRUE)
+							lrn = learners$create_learner(targets, baselrn, NULL, self$task_type, self$decision, TRUE)
 							res = mlr::resample(learner = lrn, task = self$tasks[[i]], measures = self$measures, resampling = self$ri, models = TRUE, extract = getFilteredFeatures)
 							self$single_results[[result_idx]] = self$get_model_results(res, self$tasks[[i]])
 							result_idx = result_idx + 1
@@ -117,7 +115,7 @@ MM_Single = R6::R6Class("MM_Single",
 							for (baselrn in base_learners) {
 								if (bitwAnd(active_learners, baselrn$code) || bitwAnd(active_learners, LRN_ALL_MODELS)) {
 									targets = getTaskTargetNames(self$tasks[[i]])
-									lrn = learners$create_learner(targets, baselrn, filt, self$model_type, self$decision, TRUE)
+									lrn = learners$create_learner(targets, baselrn, filt, self$task_type, self$decision, TRUE)
 									res = mlr::resample(learner = lrn, task = self$tasks[[i]], measures = self$measures, resampling = self$ri, models = TRUE, extract = getFilteredFeatures)
 									self$single_results[[result_idx]] = self$get_model_results(res, self$tasks[[i]])
 									result_idx = result_idx + 1
